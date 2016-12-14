1282262415229f819ffbef555ac879cccb1d664e
gnnpls <gnnpls_nikos@hotmail.com>
2016-12-14 13:23:36 +0200

    changes

--- motivar.php
+++ motivar.php
@@ -3,7 +3,7 @@
 Plugin Name: Wordpress Admin tools
 Plugin URI: https://www.motivar.io
 Description: Hide unwanted texts for clients and run custom php codes and shortcodes (for developers mostly)
-Version: 1.3.12
+Version: 1.3.13
 Author: Giannopoulos Nikolaos, Anastasiou Kwnstantinos
 Author URI: https://www.motivar.io
 Text Domain:       github-updater
